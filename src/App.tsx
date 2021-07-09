6ace7fd81b81426b3e6b2a42972af2f8d3cf0db0
Giovana Yokota <gnyokota@gmail.com>
2021-07-09 15:02:55 +0200

    pokemon test done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import Dropdown from "./component/Dropdown";
-import Todo from "./component/Todo";
 
 function App() {
   const options = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"];
-  const todos = [
-    { id: 1, title: "Wash the dishes", completed: false },
-    { id: 2, title: "Clean the kitchen", completed: true },
-  ];
 
   return (
     <div className="App">
       <Dropdown title={"Select your pokemon"} options={options} />
-      {todos.map((todo) => (
-        <Todo todo={todo} />
-      ))}
     </div>
   );
 }
